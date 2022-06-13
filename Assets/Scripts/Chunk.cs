0e9fe3d479f39e6274c83d4bbdfd9bcb8a059e64
curaai00 <curaai00@gmail.com>
2022-06-13 19:45:32 +0900

    refac: Seperate rendering part from chunk to chunkrenderer

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -22,30 +22,21 @@ public class Chunk
         set => gameObj.SetActive(value);
     }
 
-    protected List<Vector3> verts = new List<Vector3>();
-    protected List<int> tris = new List<int>();
-    protected List<Vector2> uvs = new List<Vector2>();
-    protected MeshRenderer meshRenderer;
-    protected MeshFilter meshFilter;
-    protected MeshCollider meshCollider;
+    public ChunkRenderer renderer;
 
     public bool Initialized { get; private set; }
     public Chunk(ChunkCoord coord, World world)
     {
-
         this.world = world;
         this.coord = coord;
 
         gameObj = new GameObject();
-        meshRenderer = gameObj.AddComponent<MeshRenderer>();
-        meshFilter = gameObj.AddComponent<MeshFilter>();
-        meshCollider = gameObj.AddComponent<MeshCollider>();
-        meshRenderer.material = world.BlockTable.material;
-
         gameObj.name = $"Chunk [{coord.x}, {coord.z}]";
         transform.SetParent(world.transform);
         transform.position = new Vector3(coord.x * Width, 0f, coord.z * Width);
+
         Initialized = false;
+        renderer = new ChunkRenderer(this, world.BlockTable);
     }
 
     public void Init()
@@ -59,41 +50,8 @@ public class Chunk
         if (Initialized)
             return;
         GenerateBlocks();
-        UpdateChunk();
+        renderer.RefreshMesh();
         Initialized = true;
-    }
-
-    protected void UpdateChunk()
-    {
-        ClearMesh();
-
-        foreach (var pos in BlockFullIterator())
-        {
-            if (GetBlock(pos).isSolid)
-                UpdateMeshBlock(pos);
-        }
-
-        CreateMesh();
-    }
-
-    protected void UpdateMeshBlock(in Vector3Int inChunkCoord)
-    {
-        var block = GetBlock(inChunkCoord);
-
-        for (int faceIdx = 0; faceIdx < VoxelData.FACE_COUNT; faceIdx++)
-        {
-            if (!IsSolidBlock(inChunkCoord + VoxelData.SurfaceNormal[faceIdx]))
-            {
-                int vertIdx = verts.Count;
-                for (int i = 0; i < 4; i++)
-                    verts.Add(inChunkCoord + VoxelData.Verts[VoxelData.Tris[faceIdx, i]]);
-
-                foreach (var i in VoxelData.TriIdxOrder)
-                    tris.Add(vertIdx + i);
-
-                uvs.AddRange(world.BlockTable.GetTextureUv(block.id, (VoxelFace)faceIdx));
-            }
-        }
     }
 
     public void EditBlock(List<BlockMod> mods)
@@ -104,7 +62,7 @@ public class Chunk
             {
                 var faceCoord = coord + VoxelData.SurfaceNormal[faceIdx];
                 if (!IsVoxelInChunk(faceCoord))
-                    world.GetChunk(chunkPos + faceCoord)?.UpdateChunk();
+                    world.GetChunk(chunkPos + faceCoord)?.renderer.RefreshMesh();
             }
         }
 
@@ -114,34 +72,9 @@ public class Chunk
             BlockMap[coord.x, coord.y, coord.z] = mod.block;
             UpdateSurroundedChunks(coord);
         }
-        UpdateChunk();
+        renderer.RefreshMesh();
     }
 
-    protected void CreateMesh()
-    {
-        Mesh mesh = new Mesh();
-        mesh.vertices = verts.ToArray();
-        mesh.triangles = tris.ToArray();
-        mesh.uv = uvs.ToArray();
-
-        mesh.RecalculateNormals();
-
-        meshFilter.mesh = mesh;
-    }
-    protected void ClearMesh()
-    {
-        verts.Clear();
-        tris.Clear();
-        uvs.Clear();
-    }
-
-    public static IEnumerable<Vector3Int> BlockFullIterator()
-    {
-        for (int x = 0; x < Width; x++)
-            for (int y = 0; y < Height; y++)
-                for (int z = 0; z < Width; z++)
-                    yield return new Vector3Int(x, y, z);
-    }
 
     public bool IsSolidBlock(in Vector3Int vp)
     {
@@ -151,4 +84,12 @@ public class Chunk
 
     public Block GetBlock(in Vector3Int v) => BlockMap[v.x, v.y, v.z];
     protected bool IsVoxelInChunk(in Vector3Int v) => v == Vector3Int.Max(Vector3Int.zero, Vector3Int.Min(ChunkShape, v));
+
+    public static IEnumerable<Vector3Int> BlockFullIterator()
+    {
+        for (int x = 0; x < Width; x++)
+            for (int y = 0; y < Height; y++)
+                for (int z = 0; z < Width; z++)
+                    yield return new Vector3Int(x, y, z);
+    }
 }
